ce128a28a994757e3f1fc98e72f292231a234b55
Lahtak <lahtakk@gmail.com>
2026-09-10 00:01:30 +0600

    Add files via upload

---
+++ pr1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class pr1
+{
+    static void Main()
+    {
+        long N = long.Parse(Console.ReadLine());
+        string[] lwk = Console.ReadLine().Split();
+        int L = int.Parse(lwk[0]);
+        int W = int.Parse(lwk[1]);
+        int K = int.Parse(lwk[2]);
+
+        long S = 2 * N * (L + W + K) + W * N * (N - 1);
+        Console.WriteLine(S);
+    }
+}
